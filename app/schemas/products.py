aadc4b2917b38dd7d9db837f6204c951334cf66c
siddhart <s.vikram@enact-systems.com>
2024-03-05 21:21:03 +0530

    added ProductSchema

---
+++ app/schemas/products.py
@@ -0,0 +1,13 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+# Pydantic Schema for Product
+class ProductSchema(BaseModel):
+    id: Optional[int] = None
+    name: str
+    price: int
+    seller_id: int
+
+    class Config:
+        orm_mode = True
