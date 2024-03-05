310cc18e47c7532fa98aad959afd869e010ccea7
siddhart <s.vikram@enact-systems.com>
2024-03-05 22:06:32 +0530

    update user router and user services

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,17 +1,17 @@
 from fastapi import APIRouter, Depends, status
-from app.schemas.users import User
+from app.schemas.users import UserSchema
 from app.database.connection import get_db
 from sqlalchemy.orm import Session
-from app.services.users import create_user, get_user_by_id
+from app.services import users as user_services
 
 user_router = APIRouter(tags=["users"])
 
 
-@user_router.post("/create_user", status_code=status.HTTP_201_CREATED, response_model=User)
-def create(user: User, db: Session = Depends(get_db)):
-    return create_user(db, user)
+@user_router.post("/create_user", status_code=status.HTTP_201_CREATED, response_model=UserSchema)
+def create(user: UserSchema, db: Session = Depends(get_db)):
+    return user_services.create_user(db, user)
 
 
-@user_router.get("/get_user/{id}", status_code=status.HTTP_200_OK, response_model=User)
+@user_router.get("/get_user/{id}", status_code=status.HTTP_200_OK, response_model=UserSchema)
 def get(id, db: Session = Depends(get_db)):
-    return get_user_by_id(db, id)
+    return user_services.get_user_by_id(db, id)
