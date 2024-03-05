69afa84a47447db08fee176243b30cab921bf69a
siddhart <s.vikram@enact-systems.com>
2024-03-05 21:20:28 +0530

    added create product services helper

---
+++ app/services/products.py
@@ -0,0 +1,16 @@
+from app.models.products import Product
+from app.schemas.products import ProductSchema
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
+
+
+def create_product(db: Session, product: ProductSchema):
+    db_product = Product(
+        name=product.name,
+        price=product.price,
+        seller_id=product.seller_id
+    )
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
