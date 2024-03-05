31c7af4f7363eab6abab27d2f72166d7aea4b897
siddhart <s.vikram@enact-systems.com>
2024-03-05 21:32:29 +0530

    update helper service refrance

--- app/routes/products.py
+++ app/routes/products.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status
 from app.schemas.products import ProductSchema
 from app.database.connection import get_db
 from sqlalchemy.orm import Session
-from app.services.products import create_product, get_all_products
+from app.services import products as product_services
 from typing import List
 
 product_router = APIRouter(tags=["products"])
@@ -10,9 +10,9 @@ product_router = APIRouter(tags=["products"])
 
 @product_router.post("/create_product", status_code=status.HTTP_201_CREATED, response_model=ProductSchema)
 def create(product: ProductSchema, db: Session = Depends(get_db)):
-    return create_product(db=db, product=product)
+    return product_services.create_product(db=db, product=product)
 
 
 @product_router.get("/list/all", status_code=status.HTTP_200_OK, response_model=List[ProductSchema])
 def get(db: Session = Depends(get_db)):
-    return get_all_products(db)
+    return product_services.get_all_products(db)
