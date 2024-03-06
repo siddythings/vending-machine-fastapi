63e42696535b68176359bf1cb08fa4fdedd9547e
siddhart <s.vikram@enact-systems.com>
2024-03-06 14:43:03 +0530

    updated purchase service with improved logic

--- app/services/purchase.py
+++ app/services/purchase.py
@@ -1,4 +1,4 @@
-from app.models.deposit import Deposit
+from app.schemas.deposit import DepositSchema
 from app.schemas.products import ProductSchema
 from app.schemas.purchase import PurchaseSchema, PurchaseResponse
 from app.models.services.deposit import DepositModelService
@@ -18,24 +18,26 @@ class PurchaseService:
     def purchase_product(self, purchase: PurchaseSchema):
         product = self.product_model_service.get_product_by_id(
             id=purchase.product_id)
-        user_balance = self.deposit_model_service.get_user_current_deposit(
-            user_id=purchase.user_id)
+        user_deposit_balance = DepositSchema.from_orm(
+            self.user_current_deposit(purchase=purchase))
 
         total_cost = product.price * purchase.quantity
 
-        if total_cost > user_balance:
+        if total_cost > user_deposit_balance.amount:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST, detail="Insufficient funds")
-
+        user_deposit_balance.amount -= total_cost
         purchase = self.purchase_model_service.record_purchase(
             purchase=purchase)
+
+        self.update_deposit_amount(user_deposit_balance)
 
         return PurchaseResponse(
             user_id=purchase.user_id,
             total_spent=total_cost,
             products_purchased=ProductSchema.from_orm(product),
             quantity=purchase.quantity,
-            change=self.return_change(user_balance - total_cost)
+            change=self.return_change(user_deposit_balance.amount - total_cost)
         )
 
     def return_change(self, balance):
@@ -50,3 +52,10 @@ class PurchaseService:
                     break
 
         return sorted(change, reverse=True)
+
+    def user_current_deposit(self, purchase: PurchaseSchema):
+        return self.deposit_model_service.get(
+            user_id=purchase.user_id)
+
+    def update_deposit_amount(self, deposit: DepositSchema):
+        return self.deposit_model_service.create_or_update(deposit=deposit)
