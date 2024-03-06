1306bfe4eaa73a3e5f8508c2c6a41efce9385caf
siddhart <s.vikram@enact-systems.com>
2024-03-06 18:33:00 +0530

    Update deposit Test cases

--- tests/test_deposit.py
+++ tests/test_deposit.py
@@ -17,38 +17,52 @@ def generate_random_string():
 
 
 OWNER_USER_ID = 1
-BUYER_USER_ID = 3
+BUYER_USER_ID = 2
 
 user1 = generate_random_string()
 user2 = generate_random_string()
-initial_username_owner = user1
-initial_role_owner = "owner"
 
 initial_username_buyer = user2
 initial_role_buyer = "buyer"
 
 
+def test_create_buyer_user(request):
+    response = client.post(
+        "/user/create_user",
+        json={
+            "username": initial_username_buyer,
+            "role": initial_role_buyer
+        },
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.json()["username"] == user2
+    assert response.json()["role"] == "buyer"
+    request.config.cache.set("buyer_user_id", response.json()["id"])
+
+
 @pytest.mark.dependency()
 def test_create_deposit(request):
+    buyer_id = request.config.cache.get("buyer_user_id", None)
     response = client.post(
         "/deposit",
         json={
-            "user_id": BUYER_USER_ID,
+            "user_id": buyer_id,
             "amount": 100
         },
     )
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json()["amount"] == 100
-    assert response.json()["user_id"] == BUYER_USER_ID
+    assert response.json()["user_id"] == buyer_id
     request.config.cache.set("user_id", response.json()["user_id"])
 
 
 @pytest.mark.dependency()
 def test_create_deposit_invalid_amount(request):
+    buyer_id = request.config.cache.get("buyer_user_id", None)
     response = client.post(
         "/deposit",
         json={
-            "user_id": BUYER_USER_ID,
+            "user_id": buyer_id,
             "amount": 99
         },
     )
@@ -57,6 +71,6 @@ def test_create_deposit_invalid_amount(request):
 
 @pytest.mark.dependency(depends=["test_create_deposit"])
 def test_get_deposit_detail(request):
-    user_id = request.config.cache.get("user_id", None)
-    response = client.get(f"/deposit/{user_id}")
+    buyer_id = request.config.cache.get("buyer_user_id", None)
+    response = client.get(f"/deposit/{buyer_id}")
     assert response.status_code == status.HTTP_200_OK
