d50663a372cf31e3618e3c875ec502fa54a98839
siddhart <s.vikram@enact-systems.com>
2024-03-06 15:37:54 +0530

    added cors middleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, status
+from fastapi.middleware.cors import CORSMiddleware
 from app.models.users import User
 from app.models.products import Product
 from app.models.deposit import Deposit
@@ -12,6 +13,17 @@ from app.routes.reset_deposit import reset_deposit_router
 from app.schemas.users import UserSchema
 
 app = FastAPI()
+
+# To handle security on origins
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router=user_router, prefix="/user")
 app.include_router(router=product_router, prefix="/product")
