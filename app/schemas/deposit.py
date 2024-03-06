298be0b2af3bb343142dc8cdab8e1e417ca54ff4
siddhart <s.vikram@enact-systems.com>
2024-03-06 11:06:03 +0530

    added deposit schema

---
+++ app/schemas/deposit.py
@@ -0,0 +1,12 @@
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+# Pydantic Schema for Deposit
+class DepositSchema(BaseModel):
+    id: Optional[int] = None
+    user_id: int
+    amount: int
+
+    class Config:
+        orm_mode = True
