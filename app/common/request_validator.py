f8d2e2ce2348ee38f1d5e841e3b3a6cd6d12e96a
siddhart <s.vikram@enact-systems.com>
2024-03-06 14:02:33 +0530

    update buyer only from purchase schema

--- app/common/request_validator.py
+++ app/common/request_validator.py
@@ -1,5 +1,8 @@
 from app.schemas.products import ProductSchema
+from app.schemas.deposit import DepositSchema
+from app.schemas.purchase import PurchaseSchema
 from app.services import products as product_services
+from app.services import users as user_services
 from sqlalchemy.orm import Session
 from app.database.connection import get_db
 from fastapi import HTTPException, status, Depends
@@ -14,5 +17,31 @@ def owner_only(func):
                 status_code=status.HTTP_403_FORBIDDEN, detail="Sorry!! Owner only can perform this action")
 
         return func(id=id, product=product, db=db)
+
+    return wrapper
+
+
+def buyer_only(func):
+    def wrapper(deposit: DepositSchema, db: Session = Depends(get_db)):
+        user_details = user_services.get_user_by_id(id=deposit.user_id, db=db)
+
+        if user_details.role != "buyer":
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN, detail="Sorry!! Buyer only can perform this action")
+
+        return func(deposit=deposit, db=db)
+
+    return wrapper
+
+
+def buyer_only_purchase(func):
+    def wrapper(purchase: PurchaseSchema, db: Session = Depends(get_db)):
+        user_details = user_services.get_user_by_id(id=purchase.user_id, db=db)
+
+        if user_details.role != "buyer":
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN, detail="Sorry!! Buyer only can perform this action")
+
+        return func(purchase=purchase, db=db)
 
     return wrapper
