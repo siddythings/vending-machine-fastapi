539cf9f81b74d429be657a1ade2393f02c515eb4
siddhart <s.vikram@enact-systems.com>
2024-03-06 18:27:27 +0530

    update user test case

--- tests/test_users.py
+++ tests/test_users.py
@@ -25,22 +25,6 @@ initial_username_buyer = user2
 initial_role_buyer = "buyer"
 
 
-@pytest.mark.dependency()
-def test_create_owner_user(request):
-    response = client.post(
-        "/user/create_user",
-        json={
-            "username": initial_username_owner,
-            "role": initial_role_owner
-        },
-    )
-    assert response.status_code == status.HTTP_201_CREATED
-    assert response.json()["username"] == user1
-    assert response.json()["role"] == "owner"
-    request.config.cache.set("user_id", response.json()["id"])
-
-
-@pytest.mark.dependency()
 def test_create_buyer_user(request):
     response = client.post(
         "/user/create_user",
@@ -52,10 +36,23 @@ def test_create_buyer_user(request):
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json()["username"] == user2
     assert response.json()["role"] == "buyer"
-    request.config.cache.set("user_id", response.json()["id"])
+    request.config.cache.set("buyer_user_id", response.json()["id"])
 
 
-@pytest.mark.dependency()
+def test_create_owner_user(request):
+    response = client.post(
+        "/user/create_user",
+        json={
+            "username": initial_username_owner,
+            "role": initial_role_owner
+        },
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.json()["username"] == user1
+    assert response.json()["role"] == "owner"
+    request.config.cache.set("owner_user_id", response.json()["id"])
+
+
 def test_dublicate_user(request):
     response = client.post(
         "/user/create_user",
@@ -67,18 +64,14 @@ def test_dublicate_user(request):
     assert response.status_code == status.HTTP_409_CONFLICT
 
 
-@pytest.mark.dependency(depends=[
-    "test_create_buyer_user"
-])
 def test_get_all_users():
     response = client.get("/user/list/all")
     assert response.status_code == status.HTTP_200_OK
     assert response.json() is not None
 
 
-@pytest.mark.dependency(depends=["test_create_buyer_user"])
 def test_get_one_user(request):
-    user_id = request.config.cache.get("user_id", None)
+    user_id = request.config.cache.get("buyer_user_id", None)
     response = client.get(f"/user/{user_id}")
     assert response.status_code == status.HTTP_200_OK
     assert response.json()["id"] == user_id
@@ -86,9 +79,8 @@ def test_get_one_user(request):
     assert response.json()["role"] == initial_role_buyer
 
 
-@pytest.mark.dependency(depends=["test_create_buyer_user"])
 def test_patch_user(request):
-    user_id = request.config.cache.get("user_id", None)
+    user_id = request.config.cache.get("buyer_user_id", None)
     temp_name = generate_random_string()
     response = client.patch(
         f"/user/update/{user_id}",
@@ -113,7 +105,7 @@ def test_patch_user(request):
     ]
 )
 def test_delete_user(request):
-    user_id = request.config.cache.get("user_id", None)
+    user_id = request.config.cache.get("buyer_user_id", None)
     response = client.delete(f"/user/delete/{user_id}")
     assert response.status_code == status.HTTP_202_ACCEPTED
     assert response.json()["detail"] == "User Deleted"
