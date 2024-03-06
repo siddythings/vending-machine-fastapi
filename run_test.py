22237f9cf0da078fa25f1650f5749c58367e4f92
siddhart <s.vikram@enact-systems.com>
2024-03-06 19:35:07 +0530

    TEST: run test file

--- run_test.py
+++ run_test.py
@@ -1,4 +1,9 @@
 import pytest
 
 if __name__ == "__main__":
-    pytest.main(["tests/"])
+    pytest.main([
+        "tests/test_users.py",
+        "tests/test_products.py",
+        "tests/test_deposit.py",
+        "tests/test_buy.py",
+    ])
