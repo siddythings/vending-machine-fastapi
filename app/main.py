1729afe865e726e7b629d08cc7d26793c344c2f4
siddhart <s.vikram@enact-systems.com>
2024-03-06 15:36:08 +0530

    added router to main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
-from typing import Union
+from fastapi import FastAPI, Depends, status
+from app.models.users import User
+from app.models.products import Product
+from app.models.deposit import Deposit
+from app.models.purchase import Purchase
 
-from fastapi import FastAPI
+from app.routes.users import user_router
+from app.routes.products import product_router
+from app.routes.deposit import deposit_router
+from app.routes.purchase import purchase_router
+from app.routes.reset_deposit import reset_deposit_router
+from app.schemas.users import UserSchema
 
 app = FastAPI()
+
+app.include_router(router=user_router, prefix="/user")
+app.include_router(router=product_router, prefix="/product")
+app.include_router(router=deposit_router, prefix="/deposit")
+app.include_router(router=purchase_router, prefix="/buy")
+app.include_router(router=reset_deposit_router, prefix="/reset")
 
 
 @app.get("/")
