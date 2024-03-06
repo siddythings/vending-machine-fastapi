d3a925788b8afb6b1d12c92f370e5a4755089dbb
siddhart <s.vikram@enact-systems.com>
2024-03-06 14:43:32 +0530

    update deposit service on deposit money

--- app/services/deposit.py
+++ app/services/deposit.py
@@ -16,7 +16,7 @@ class DepositService:
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Please Select valid Coin e.g 200, 100, 25, 10, 5")
 
-        return self.deposit_model.create(deposit=deposit)
+        return self.deposit_model.create_or_update(deposit=deposit)
 
     def reset_deposit(self, user_id):
         self.deposit_model.delete_all_deposits_by_user_id(user_id=user_id)
